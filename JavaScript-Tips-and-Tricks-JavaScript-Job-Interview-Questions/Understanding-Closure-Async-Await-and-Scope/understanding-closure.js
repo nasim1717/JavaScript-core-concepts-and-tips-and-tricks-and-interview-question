578f3abee75d7ec2e17248917f57c702a48f53c2
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-02-02 15:04:56 +0600

    add clouser and async await file

---
+++ JavaScript-Tips-and-Tricks-JavaScript-Job-Interview-Questions/Understanding-Closure-Async-Await-and-Scope/understanding-closure.js
@@ -0,0 +1,23 @@
+// function timer(n) {
+//     console.log("Start timer");
+
+//     for (var i = 0; i <= n; i++) {
+//         setTimeout(() => {
+//             console.log(i);
+//         }, i * 1000);
+//     }
+//     console.log("End timer");
+// }
+
+function timer(n) {
+    console.log("Start timer");
+
+    for (let i = 0; i <= n; i++) {
+        setTimeout(() => {
+            console.log(i);
+        }, i * 1000);
+    }
+    console.log("End timer");
+}
+
+timer(4);
